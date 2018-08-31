7bda764637f14d21dba524efec821b2557cc6c15
katekosushkina <kate.kosushkina@gmail.com>
2018-08-31 22:54:53 +0300

    Added Likes

--- js/tabs.js
+++ js/tabs.js
@@ -9,19 +9,45 @@ $(document).ready(function(){
     console.log(tab.name);
     let foodList = "";
     let activeClass = i===0?'active':'';
-    $("#myTab").append(`<li class="nav-item">
+    $("#myTab").append(`
+      <li class="nav-item">
         <a class="nav-link ${activeClass}" id="home-tab" data-toggle="tab"
         href="#tab${i}" role="tab" aria-controls="home" aria-selected="true">
         ${tab.name}</a>
-        </li>`)
-tab.foodList.map((food, i)=>{
-  foodList += `<div class="cont"><div class="name"> ${food.name} </div>
-  <img src="${food.img}">
-  </div>`
-});
-  $("#myTabContent").append(`<div class="tab-pane fade show ${activeClass}" id="tab${i}" role="tabpanel" aria-labelledby="home-tab">${foodList}</div>`)
-
+      </li>`)
+    tab.foodList.map((food, j)=>{
+      foodList +=
+      `<div class="cont">
+        <div class="name"> ${food.name}
+          <i class="far fa-heart" id="pic_${i}_${j}"></i> </div>
+        <img src="${food.img}">
+      </div>`
+    });
+    $("#myTabContent").append(
+      `<div class="tab-pane fade show ${activeClass}" id="tab${i}" role="tabpanel" aria-labelledby="home-tab">
+        ${foodList}
+      </div>`)
   });
 });
+
+$("body").on('click', ".fa-heart", function(){
+  $(this).toggleClass('far').toggleClass('fas');
+  let id = $(this).attr("id");
+  if ($(this).hasClass('fas')){
+    localStorage.setItem(id, true);
+    console.log(id, localStorage.getItem(id))
+  } else {
+    localStorage.removeItem(id);
+  }
+})
+
+$("#checkbox").on('click', function(){
+  if ($(this).prop("checked")) {
+  $(".far").closest('.cont').hide();
+} else {
+$(".far").closest('.cont').show();
+}
+})
+
 
 });

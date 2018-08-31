e00b65e1d3597e828ee4e52452ce1d02059a542c
katekosushkina <kate.kosushkina@gmail.com>
2018-08-31 22:00:00 +0300

    refactoring

--- js/tabs.js
+++ js/tabs.js
@@ -9,12 +9,17 @@ $(document).ready(function(){
     console.log(tab.name);
     let foodList = "";
     let activeClass = i===0?'active':'';
-    $("#myTab").append('<li class="nav-item">'+
-        '<a class="nav-link '+activeClass+'" id="home-tab" data-toggle="tab" href="#tab'+i+'" role="tab" aria-controls="home" aria-selected="true">'+tab.name+'</a></li>')
+    $("#myTab").append(`<li class="nav-item">
+        <a class="nav-link ${activeClass}" id="home-tab" data-toggle="tab"
+        href="#tab${i}" role="tab" aria-controls="home" aria-selected="true">
+        ${tab.name}</a>
+        </li>`)
 tab.foodList.map((food, i)=>{
-  foodList += '<div class="cont"><div class="name">'+food.name+'</div><img src="'+food.img+'"></div>'
+  foodList += `<div class="cont"><div class="name"> ${food.name} </div>
+  <img src="${food.img}">
+  </div>`
 });
-  $("#myTabContent").append('<div class="tab-pane fade show '+activeClass+'" id="tab'+i+'" role="tabpanel" aria-labelledby="home-tab">'+foodList+'</div>')
+  $("#myTabContent").append(`<div class="tab-pane fade show ${activeClass}" id="tab${i}" role="tabpanel" aria-labelledby="home-tab">${foodList}</div>`)
 
   });
 });

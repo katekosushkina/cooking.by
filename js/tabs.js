252e3a6d6caecf59c6865fea87bc09bed9ee22fc
katekosushkina <kate.kosushkina@gmail.com>
2018-09-02 19:08:13 +0300

    enterAdded

--- js/tabs.js
+++ js/tabs.js
@@ -14,7 +14,8 @@ function findRecipe(){
     data.tabs.map((tab, i)=>{
       tab.foodList.map((food, j)=>{
         var foodName = food.name;
-        if (ac===foodName){
+        if (ac===foodName){  
+          $("#currentRecipe").empty();
           $("#currentRecipe").append(
               `<button type="button" onclick="removeCurrentRecipe()">назад</button>
                 <div class="name">${food.name}
@@ -39,7 +40,79 @@ function findRecipe(){
 });
 
 };
-
+function divEnter(){
+  $("#enter").empty();
+  $("#enter").append(
+            `<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>`)
+          $("#enter").css({'position': 'relative','z-index': '999999', 'height': '1000px', 'width': '100%', 'clear': 'both', 'background-color':'white','margin-top':'30px' });
+};
+function  divRegistration(){
+  $("#enter").empty();
+  $("#enter").append(
+    `<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Email</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Password</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Address</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Address 2</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">City</label>
+      <input type="text" class="form-control" id="inputCity">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">State</label>
+      <select id="inputState" class="form-control">
+        <option selected>Choose...</option>
+        <option>...</option>
+      </select>
+    </div>
+    <div class="form-group col-md-2">
+      <label for="inputZip">Zip</label>
+      <input type="text" class="form-control" id="inputZip">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Check me out
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+`)
+   $("#enter").css({'position': 'relative','z-index': '999999', 'height': '1000px', 'width': '100%', 'clear': 'both', 'background-color':'white','margin-top':'30px' });
+};
 
 $(document).ready(function(){
 
@@ -120,6 +193,7 @@ $("body").on('click', ".foodImg", function(){
     tab.foodList.map((food, j)=>{
       let foodImg = food.img;
       if (imgSrc===foodImg){
+            $("#currentRecipe").empty();
             $("#currentRecipe").append(
                 `<button type="button" onclick="removeCurrentRecipe()">назад</button>
                 ${inf}
@@ -135,4 +209,5 @@ $("body").on('click', ".foodImg", function(){
   });
 
 });
+
 });
